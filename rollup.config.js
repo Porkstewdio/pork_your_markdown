f8174a1d6489c93336f05d181278a7aa2f1f8a95
Eastroots92 <eastroots92@gmail.com>
2018-05-20 17:19:50 +0900

    rollup -c 할 때 .js와 .min.js 파일 두개를 따로따로 빌드하는 코드

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
 import buble from 'rollup-plugin-buble';
 import minify from 'rollup-plugin-minify-es';
-export default {
+export default [{
   input: 'source/main.js',
   output : {
-    name : 'output/pumd.js',
+    file: 'output/pumd.min.js',
     format: 'iife', 
     name : "PorkUrMarkDown"
   },
   plugins: [ buble(), minify() ]
-};
+}, { 
+  input: 'source/main.js',
+  output: {
+    file: "output/pumd.js",
+    format: 'iife',
+    name : "PorkUrMarkDown"
+  },
+  plugins: [ buble()]
+}]
