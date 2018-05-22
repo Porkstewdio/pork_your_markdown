b24a4a73cebbf7d183583026b2964a00433c6d47
Jujin Kim <jk012345@gmail.com>
2018-05-23 03:02:50 +0900

    Added pattern form title tag(h1~h6) in md2html Apply md2html instead of showdown on getMarkDown

--- source/md2html.js
+++ source/md2html.js
@@ -1,8 +1,18 @@
+
+/** regex pattern */
+const pattern = [
+    [/^#{1}[ ?]/, "<h1>", "</h1>"],
+    [/^#{2}[ ?]/, "<h2>", "</h2>"],
+    [/^#{3}[ ?]/, "<h3>", "</h3>"],
+    [/^#{4}[ ?]/, "<h4>", "</h4>"],
+    [/^#{5}[ ?]/, "<h5>", "</h5>"],
+    [/^#{6}[ ?]/, "<h6>", "</h6>"],
+]
+
 /**
  * Markdown to HTML module class
  */
 export class Md2HtmlConverter {
-    rawData;
     constructor(rawData) {
         this.rawData = rawData;
     }
@@ -20,6 +30,29 @@ export class Md2HtmlConverter {
      * @returns {string} Converted HTML string
      */
     toHTML() {
-        
+        //1. Split data line by line.
+        let arrStr = this.rawData.split("\n");
+
+        //2. Find pattern using regex.
+        //3. If found pattern is one-line-pattern, replace
+        let i;
+        for(i=0; i<arrStr.length; i++) {
+            let line = arrStr[i];
+            pattern.forEach(function(ptnArr) {
+                let regexRes = ptnArr[0].exec(line);
+                if(regexRes !== undefined &&  regexRes !== null) {
+                    line = line.replace(ptnArr[0], "");
+                    line = ptnArr[1] + line + ptnArr[2];
+                }
+            });
+            arrStr[i] = line;
+        }
+
+        //4. join
+        let str = arrStr.join("");
+
+        //5. If fount pattern is multi-line-pattern, find end of pattern.
+
+        return str;
     }
 }
